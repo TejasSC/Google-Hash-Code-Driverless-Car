088e2a48e70d0ccc62bd495dc8dd7009436627d5
hindolo <hindolo@btinternet.com>
2018-03-01 20:20:19 +0000

    initial changes to Vehicle, reiqures checking

--- Vehicle.java
+++ Vehicle.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class Vehicle
 {
   private Ride[] vehicleRides;
+  int rideCount;
 
   //Empty, a vehicle initially has no rides 
   public Vehicle()
@@ -17,11 +18,42 @@ public class Vehicle
 
   public int calcScore()
   {
-    return 0;
+    
   }//calcScore
 
-  public void addRide()
+
+  public void addRide(ride addedRide)
   {
-  
+    //check for overlaps
+    for(int rideNo = 0; rideNo < rideCount; rideNo ++)
+    {
+      if(addedRide.overlaps(vehicleRides[rideNo]))
+      {//remove if they overlap
+        remove(rideNo);
+      }
+    }
+
+    int rideIndex = -1;;
+
+    for(int rideNo = rideNo-1;rideNo>=0;rideNo--)
+    {
+      if(addedRide.before(vehicleRides[rideNo]))
+      {//if the new ride will come before a current array ride, it moves the current ride forwasrd
+        vehicleRides[rideNo+1]=vehicleRides[rideNo];
+      }else if(rideIndex==-1){rideIndex=rideNo}
+    }
+
   }//addRide
+
+  private void remove(int index)
+  {
+    for(int rideNo = index;rideNo<rideCount-1;rideNo++)
+    {
+
+      vehicleRides[index] = vehicleRides[index]+1;
+      rideCount--;
+  
+    }//push all the rides back an index and decrease count
+
+  }//remove
 }
